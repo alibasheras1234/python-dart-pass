727bf1aacc8e08e871b78532414f71f5fe5ed5e1
Ali Basheer <66694679+alibasheras1234@users.noreply.github.com>
2022-04-06 23:07:06 +0300

    Done

--- Dart-pass/Q1_Dart.dart
+++ Dart-pass/Q1_Dart.dart
@@ -3,7 +3,8 @@ main() {
   const hour = DateTime.now().hour;
   print(hour);
   
-  A variable with the const keyword is initialized at compile time and is already assigned when at runtime.
+  A variable with the const keyword is initialized at compile time and is already assigned when at runtime and
+  Here the clock is without a value because DateTime.now(). A watch does not give a value of the hour until the time it runs.
   Summary:
       The value of the static variable must be given while defining the variable.
       ex :
